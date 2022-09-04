bbb7321c3fda68395911e35efc634a26e3d9e3bd
Nataliia Horb <nightangnata@gmail.com>
2022-09-04 22:56:37 +0200

    newCommit

---
+++ src/main/java/org/example/Plauer.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import lombok.Data;
+
+public abstract class Plauer implements GameConstants  {
+    private String name;
+    private int health;
+    private int superPower;
+    private boolean isLossHealth;
+
+    private boolean isNewSuperPower;
+    private boolean isEffectiveBlow;
+
+    public Player(String name, int superPower) {
+        this.name = name;
+        this.health = PLAYER_HEALTH_AT_START;
+        this.superPower = superPower;
+    }
+
+    public void blowResponse(Player enemy) {
+        if (enemy.isEffectiveBlow) {
+            if (this.superPower > 0) {
+                this.superPower--;
+                this.isLossHealth = false;
+            } else {
+                this.health -= 10;
+                this.isLossHealth = true;
+            }
+        } else {
+            this.isLossHealth = false;
+        }
+    }
+
+    public abstract void hitEnemy();
+}
